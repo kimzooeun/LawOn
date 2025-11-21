bd4ab5a0c4133e41e3b7a984d40dd1b89a8c2579
kimzooeun <asdo6863@naver.com>
2025-11-21 15:30:16 +0900

    캐시 기능 활성화

--- backend/src/main/java/com/prinCipal/chatbot/DemoApplication.java
+++ backend/src/main/java/com/prinCipal/chatbot/DemoApplication.java
@@ -3,7 +3,9 @@ package com.prinCipal.chatbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableCaching // 캐시 기능 활성화
 @SpringBootApplication
 public class DemoApplication {
