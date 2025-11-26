ca44cf9204ec30b54b589c89487ccefceed0f2fa
jiuu <wldb2065@gmail.com>
2025-11-26 16:13:59 +0900

    수정

--- frontend/front/admin/js/logs.js
+++ frontend/front/admin/js/logs.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const token = TokenManager.getAccessToken();
 
   if (!token) {
-    window.location.href = "/admin/login/";
+    window.location.href = "/admin/login";
     return;
   }
 
