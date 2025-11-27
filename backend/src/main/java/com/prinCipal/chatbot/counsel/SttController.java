a8a2603c3a12652581ad9d9b42562e9bc0199571
마이쮸 <121669011+jiyouuu@users.noreply.github.com>
2025-11-27 23:29:15 +0900

    수정

--- backend/src/main/java/com/prinCipal/chatbot/counsel/SttController.java
+++ backend/src/main/java/com/prinCipal/chatbot/counsel/SttController.java
@@ -12,7 +12,8 @@ import software.amazon.awssdk.services.s3.model.*;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.*;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.core.sync.ResponseBytes;
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 
 import java.time.Duration;
 import java.util.Base64;
